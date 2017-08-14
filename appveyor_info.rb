6bd71b7b3bdbd70ce15c9202e4c5705661586158
MSP-Greg <MSP-Greg@users.noreply.github.com>
2017-08-14 14:04:19 -0500

    Formatting & std-lib load checks

--- appveyor_info.rb
+++ appveyor_info.rb
@@ -1,7 +1,7 @@
 require "rbconfig"
 
 module VersInfo
-  @@col_wid = [28,15,15,40]
+  @@col_wid = [34, 14, 17, 26, 10, 16]
   
   class << self
   
@@ -12,13 +12,13 @@ module VersInfo
       gcc = RbConfig::CONFIG["CC_VERSION_MESSAGE"] ?
         RbConfig::CONFIG["CC_VERSION_MESSAGE"][/\A.+?\n/] : 'unknown'
       puts "gcc info: #{gcc}\n"
-      first('rubygems'  , 'Gem::VERSION'  , 15)  { Gem::VERSION   }
+      first('rubygems'  , 'Gem::VERSION'  , 18)  { Gem::VERSION     }
       puts
-      first('bigdecimal', 'BigDecimal.ver', 15)  { BigDecimal.ver }
-      first('gdbm'      , 'GDBM::VERSION' , 15)  { GDBM::VERSION  }
-      first('json'      , 'JSON::VERSION' , 15)  { JSON::VERSION  }
+      first('bigdecimal', 'BigDecimal.ver', 18)  { BigDecimal.ver   }
+      first('gdbm'      , 'GDBM::VERSION' , 18)  { GDBM::VERSION    }
+      first('json'      , 'JSON::VERSION' , 18)  { JSON::VERSION    }
       puts
-      if first('openssl', 'OpenSSL::VERSION', 0)    { OpenSSL::VERSION }
+      if first('openssl', 'OpenSSL::VERSION', 0) { OpenSSL::VERSION }
         additional('OPENSSL_VERSION'        , 0, 4) { OpenSSL::OPENSSL_VERSION }
         if OpenSSL.const_defined?(:OPENSSL_LIBRARY_VERSION)
           additional('OPENSSL_LIBRARY_VERSION', 0, 4) { OpenSSL::OPENSSL_LIBRARY_VERSION }
@@ -29,28 +29,29 @@ module VersInfo
           OpenSSL::SSL::SSLContext::METHODS.reject { |e| /client|server/ =~ e }.sort.join(' ')
         }
         puts
-        additional_file('OpenSSL::X509::DEFAULT_CERT_FILE'    , 3, 4) { OpenSSL::X509::DEFAULT_CERT_FILE }
-        additional_file('OpenSSL::X509::DEFAULT_CERT_DIR'     , 3, 4) { OpenSSL::X509::DEFAULT_CERT_DIR }
-        additional_file('OpenSSL::Config::DEFAULT_CONFIG_FILE', 3, 4) { OpenSSL::Config::DEFAULT_CONFIG_FILE }
+        additional_file('X509::DEFAULT_CERT_FILE'    , 0, 4) { OpenSSL::X509::DEFAULT_CERT_FILE }
+        additional_file('X509::DEFAULT_CERT_DIR'     , 0, 4) { OpenSSL::X509::DEFAULT_CERT_DIR }
+        additional_file('Config::DEFAULT_CONFIG_FILE', 0, 4) { OpenSSL::Config::DEFAULT_CONFIG_FILE }
+        puts
+        additional_file("ENV['SSL_CERT_FILE']"       , 0, 4) { ENV['SSL_CERT_FILE'] }
+        additional_file("ENV['SSL_CERT_DIR']"        , 0, 4) { ENV['SSL_CERT_DIR']  }
+        additional_file("ENV['OPENSSL_CONF']"        , 0, 4) { ENV['OPENSSL_CONF']  }
+        
       end
       puts
 
-      double('psych', 'Psych::VERSION', 'LIBYAML_VERSION', 0) { [Psych::VERSION, Psych::LIBYAML_VERSION] }
-      unless first('readline', 'Readline::VERSION (ext)', 0) { Readline::VERSION }
-        first('rb-readline', 'Readline::VERSION (gem)'  , 0) { Readline::VERSION }
+      double('psych', 'Psych::VERSION', 'LIBYAML_VERSION', 3, 1, 2) { [Psych::VERSION, Psych::LIBYAML_VERSION] }
+      unless first('readline', 'Readline::VERSION (ext)', 3) { Readline::VERSION }
+        first('rb-readline', 'Readline::VERSION (gem)'  , 3) { Readline::VERSION }
       end
-      double('zlib', 'Zlib::VERSION', 'ZLIB_VERSION', 0) { [Zlib::VERSION, Zlib::ZLIB_VERSION] }
+      double('zlib', 'Zlib::VERSION', 'ZLIB_VERSION', 3, 1, 2) { [Zlib::VERSION, Zlib::ZLIB_VERSION] }
       puts
-      loads?('socket', 'Socket')
-      loads?('win32/registry', 'Win32::Registry')
-      loads?('win32ole', 'WIN32OLE')
+      puts "\n#{'-' * 56} Load Test"
+      loads2?('dbm'   , 'DBM'   , 'win32/registry', 'Win32::Registry', 4)
+      loads2?('digest', 'Digest', 'win32ole'      , 'WIN32OLE'       , 4)
+      loads2?('fiddle', 'Fiddle', 'zlib'          , 'Zlib'           , 4)
+      loads1?('socket', 'Socket', 4)
       
-      puts "\n#{'-' * 45} ENV Info"
-      puts "SSL_CERT_FILE #{env_file_exists 'SSL_CERT_FILE'}"
-
-      puts "SSL_CERT_DIR  #{env_file_exists 'SSL_CERT_DIR'}"
-      puts "OPENSSL_CONF  #{env_file_exists 'OPENSSL_CONF'}"
-
       gem_list
 
       puts "\n#{'-' * 110}"
@@ -58,39 +59,56 @@ module VersInfo
 
   private
     
-    def loads?(req, str)
+    def loads1?(req, str, idx)
       begin
         require req
-        puts "#{str.ljust(@@col_wid[0])}  Ok"
-      rescue
-        puts "#{str.ljust(@@col_wid[0])}  Does not load!"
+        puts "#{str.ljust(@@col_wid[idx])}  Ok"
+      rescue LoadError
+        puts "#{str.ljust(@@col_wid[idx])}  Does not load!"
       end
     end
-  
+
+    def loads2?(req1, str1, req2, str2, idx)
+      begin
+        require req1
+        str = "#{str1.ljust(@@col_wid[idx])}  Ok".ljust(@@col_wid[0])
+      rescue LoadError
+        str = "#{str1.ljust(@@col_wid[idx])}  Does not load!".ljust(@@col_wid[0])
+      end
+      begin
+        require req2
+        puts str + "#{str2.ljust(@@col_wid[idx+1])}  Ok"
+      rescue LoadError
+        puts str + "#{str2.ljust(@@col_wid[idx+1])}  Does not load!"
+      end
+    end
+
     def first(req, text, idx)
       col = idx > 10 ? idx : @@col_wid[idx]
       require req
-      puts "#{text.ljust(col)}  #{yield}"
+      puts "#{text.ljust(col)}#{yield}"
       true
     rescue LoadError
-      puts "#{text.ljust(col)}  NOT FOUND!"
+      puts "#{text.ljust(col)}NOT FOUND!"
       false
     end
     
     def additional(text, idx, indent = 0)
       fn = yield
-      puts "#{(' ' * indent + text).ljust(@@col_wid[idx])}  #{fn}"
+      puts "#{(' ' * indent + text).ljust(@@col_wid[idx])}#{fn}"
     rescue LoadError
     end
 
     def additional_file(text, idx, indent = 0)
       fn = yield
-      if /\./ =~ File.basename(fn)
+      if fn.nil?
+        found = 'No ENV key'
+      elsif /\./ =~ File.basename(fn)
         found = File.exist?(fn) ? File.mtime(fn).utc.strftime('File Dated %F') : 'File Not Found!      '
       else
         found = Dir.exist?(fn) ? 'Dir  Exists          ' : 'Dir  Not Found!      '
       end
-      puts "#{(' ' * indent + text).ljust(@@col_wid[idx])}  #{found}  #{fn}"
+      puts "#{(' ' * indent + text).ljust(@@col_wid[idx])}#{found}  #{fn}"
     rescue LoadError
     end
 
@@ -106,13 +124,13 @@ module VersInfo
       end
     end
     
-    def double(req, text1, text2, idx)
+    def double(req, text1, text2, idx1, idx2, idx3)
       require req
       val1, val2 = yield
-      puts "#{text1.ljust(@@col_wid[idx]  )}  #{val1.ljust(@@col_wid[idx+1])}" \
-           "#{text2.ljust(@@col_wid[idx+2])}  #{val2}"
+      puts "#{text1.ljust(@@col_wid[idx1])}#{val1.ljust(@@col_wid[idx2])}" \
+           "#{text2.ljust(@@col_wid[idx3])}#{val2}"
     rescue LoadError
-      puts "#{text1.ljust(@@col_wid[idx])}  NOT FOUND!"
+      puts "#{text1.ljust(@@col_wid[idx1])}NOT FOUND!"
     end
 
     def gem_list
