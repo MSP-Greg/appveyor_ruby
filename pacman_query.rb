7edf687a826b21aef15139dfab8c294d7d1bbb32
MSP-Greg <MSP-Greg@users.noreply.github.com>
2019-04-09 10:55:02 -0500

    Update pacman_query.rb [skip ci]

--- pacman_query.rb
+++ pacman_query.rb
@@ -8,6 +8,9 @@ module Msys2Info
   YELLOW = "\e[33m"
   RESET = "\e[0m"
 
+  PRE64 = /\Amingw-w64-x86_64-/
+  PRE32 = /\Amingw-w64-i686-/
+
   class << self
     def run
       case ARGV[0]
@@ -19,12 +22,16 @@ module Msys2Info
         d4 = "\u2015".dup.force_encoding('utf-8') * 4
       end
 
+      if !ENV['APPVEYOR'] and File.exist? "C:/msys64/usr/bin/bash.exe"
+        ENV['PATH'] = "C:/msys64/usr/bin;#{ENV['PATH']}"
+      end
+
       ary = `bash -lc "pacman -Q"`
 
       hsh = ary.split(/[\r\n]+/).group_by { |e|
         case e
-        when /\Amingw-w64-i686-/   then :i686
-        when /\Amingw-w64-x86_64-/ then :x64
+        when PRE32 then :i686
+        when PRE64 then :x64
         else :msys2
         end
       }
@@ -39,20 +46,21 @@ module Msys2Info
         x, i = 0,0
         0.upto(max_len) { |j|
           # get package base name
-          x64  = hsh[:x64][x]  ? (hsh[:x64][x]  || '').split(' ')[0].split('-').last : ''
-          i686 = hsh[:i686][i] ? (hsh[:i686][i] || '').split(' ')[0].split('-').last : ''
+          x64  = name_64 hsh[:x64][x]
+          i686 = name_32 hsh[:i686][i]
+          # puts "#{x64.ljust 30}  #{i686.ljust 30}"  
           if x64 != i686
-            if x64 == hsh[:i686][i+1] ? (hsh[:i686][i+1] || '').split(' ')[0].split('-').last : '' 
+            if x64 < name_32(hsh[:i686][i+1])
               puts "#{(hsh[:x64][x] || '')}"
-              i -= 1
-            elsif i686 == hsh[:x64][x+1] ? (hsh[:x64][x+1]  || '').split(' ')[0].split('-').last : ''
+              x += 1
+            elsif i686 < name_64(hsh[:x64][x+1])
               puts "#{''.ljust(59)} #{hsh[:i686][i] || ''}"        
-              x -= 1
+              i += 1
             end
           else
             puts "#{(hsh[:x64][x] || '').ljust(59)} #{hsh[:i686][i] || ''}"
+            x += 1 ; i += 1
           end
-          x += 1 ; i += 1
         }
       end
 
@@ -69,6 +77,15 @@ module Msys2Info
       puts "#{YELLOW}#{str}#{RESET}"
     end
 
+    def name_64(pkg)
+      return '' unless pkg
+      pkg.split(' ')[0].sub(PRE64, '')
+    end
+    
+    def name_32(pkg)
+      return '' unless pkg
+      pkg.split(' ')[0].sub(PRE32, '')
+    end
   end
 end
 Msys2Info.run
